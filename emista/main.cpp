e75c8335aa3ddceb9543c5663f912d019c33613d
longnhat2k9 <phannguyenlongnhat2k9@gmail.com>
2025-02-04 20:11:53 +0700

    New Solution Problem 4: Esum (77/100 +2AC)

--- emista/main.cpp
+++ emista/main.cpp
@@ -1,13 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int M = 1e9;
+const int M = 1e6;
+const int N = 1e5;
 
-int main(){
-    freopen("emista.inp", "r", stdin);
-    freopen("emista.out", "w", stdout);
-
-    long long n, k; cin >> n >> k;
+void cs1(long long &n, long long &k)
+{
     vector<pair<long long, long long>>a(n);
     for(int i = 0; i < n; i++)
     {
@@ -18,7 +16,6 @@ int main(){
 
     int l = 0;
     long long mx, sum; mx = sum = 0;
-    k = 2*k;
     
     for(int r = 0; r < n; r++)
     {
@@ -34,4 +31,40 @@ int main(){
     }
 
     cout << mx;
+}
+
+void cs2(long long &n, long long &k)
+{
+    vector<long long>a(M, 0); long long mxn = 0;
+    for(int i = 0; i < n; i++)
+    {
+        long long x, y; cin >> x >> y;
+        a[x] = y; mxn = max(mxn, x);
+    }
+
+    k++;
+
+    long long sum = 0, mx = 0;
+    for(int i = 0; i < k; i++) sum += a[i];
+    mx = sum;
+
+    for(int i = k; i <= mxn; i++)
+    {
+        sum -= a[i - k];
+        sum += a[i];
+        mx = max(sum, mx);
+    }
+
+    cout << mx;
+}
+
+int main(){
+    freopen("emista.inp", "r", stdin);
+    freopen("emista.out", "w", stdout);
+
+    long long n, k; cin >> n >> k;
+    k = 2*k;
+
+    if(n <= N) cs1(n, k);
+    else cs2(n, k);
 }
